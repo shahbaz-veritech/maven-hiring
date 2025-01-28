97c855933d4d35b37cc59ddcb619f351e7737600
Shahbaz <alamshahbaz287@gmail.com>
2025-01-28 22:41:37 +0530

    initial configuration

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: false,
+  theme: {
+    colors: {
+      primary: "#f9f9f9",
+      // secondary: "#ecc94b",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
